e59f04b98c1bd816a35d4bdd96945a4a672d8e04
Kirill <kirill.potapenkov@mail.ru>
2022-12-04 18:48:19 +0300

    add get_by_date

--- func_1.py
+++ func_1.py
@@ -1,10 +1,27 @@
 import csv
 
 
-f = open("all_stocks_5yr.csv", "r", encoding="utf-8")
-reader = csv.DictReader(f, delimiter=',', quotechar='|')
+def select_sorted(sort_columns="high", limit=30, group_by_name=False, order='desc', filename='dump.csv'):
+    s_c = "".join(sort_columns)
+    f_n = "".join(filename)
+    f = open("all_stocks_5yr.csv", "r", encoding="utf-8")
+    reader = csv.DictReader(f, delimiter=',', quotechar='|')
+    top10 = sorted(reader, key=lambda x: float(x[s_c] or 0), reverse=True)[:int(limit)]
 
-top10 = sorted(reader,key=lambda x: float(x["high"] or 0), reverse=True)[:10]
+    for row in top10:
+        print(f"{row['date']}|{row['open']}|{row['high']}|{row['low']}|{row['close']}|{row['volume']}|{row['Name']}")
 
-for row in top10:
-    print(f"{row['date']}|{row['open']}|{row['high']}|{row['low']}|{row['close']}|{row['volume']}|{row['Name']}")
+        """Запись в файл"""
+    f = open(f_n, "w", encoding="utf-8", newline="")
+    writer = csv.DictWriter(f, fieldnames=list(top10[0].keys()), delimiter=",", quoting=csv.QUOTE_NONE)
+    writer.writeheader()
+    for i in top10:
+        writer.writerow(i)
+
+    return top10
+
+select_sorted(sort_columns="high", limit=2, group_by_name=False, order='desc', filename='dump4.csv')
+
+select_sorted(sort_columns="low", limit=1, group_by_name=False, order='desc', filename='dump5.csv')
+
+select_sorted(sort_columns="high", limit=2, group_by_name=False, order='desc', filename='dump6.csv')
